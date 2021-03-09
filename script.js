ffbfd652c6e737c4b8ad72c243351a6f7c2988ba
kingston <kingston@cagetest.com>
2021-03-09 15:27:37 +0800

    updated model shards with api implementation

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
-const video = document.getElementById('video')
 // create element
+const video = document.getElementById('video')
 
+// array of promises with a .then() that runs after all apis are loaded
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('./models'),
+    faceapi.nets.ageGenderNet.loadFromUri('./face-api.js-master/weights')
+
+]).then(startVideo)
+
+
+// gets the video
 function startVideo() {
-    navigator.getUserMedia(
-        { video: {} },
+    navigator.getUserMedia({
+            video: {}
+        },
         stream => video.srcObject = stream,
         err => console.error(err)
     )
 }
 
-startVideo()
+
+
+
+video.addEventListener('play', () => {
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+    const displaySize = {
+        width: video.width,
+        height: video.height
+    }
+    faceapi.matchDimensions(canvas, displaySize)
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video,
+                new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks()
+            .withFaceExpressions().withAgeAndGender()
+        console.log(detections)
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+
+        faceapi.draw.drawDetections(canvas, resizedDetections)
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections) //drawsface landmarks
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+    }, 700)
+})
